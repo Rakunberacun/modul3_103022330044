140544c84c768b7baee6502f7ad3dda62aa95067
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 15:52:29 +0700

    buat bagian 1A

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace modul3_103022330044
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
